2b9bc5c424608ac6cf79d588ef8108f292a74f7f
KlaussVP <klauss_mbc@hotmail.com>
2022-02-15 10:36:57 -0300

    feat: second section initial layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,11 +5,15 @@ export default function Home() {
   return (
     <HomeContainer>
       <LandingContainer>
-        <p>Measuring <br/> everything</p>
+        <h2>Measuring <br/> everything</h2>
         <div className="dummy"></div>
       </LandingContainer>
       <PresentationContainer>
-        
+        <div className="text">
+          <h2>We are Leader <br/> in Measure Tapes</h2>
+          <p>There are 5x the <br/> circumference of planet <br/> earth in metric tapes.</p>
+        </div>
+        <div className="dummy"></div>
       </PresentationContainer>
     </HomeContainer>
   );
